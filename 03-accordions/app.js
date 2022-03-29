8df990ee847d33e705340f88713d61665a459721
adityackr <aditya09me@gmail.com>
2022-03-29 15:59:22 +0600

    sliding and icon added

--- 03-accordions/app.js
+++ 03-accordions/app.js
@@ -4,10 +4,12 @@ for (let i = 0; i < accordions.length; i++) {
 	accordions[i].addEventListener('click', function () {
 		this.classList.toggle('active');
 		const panel = this.nextElementSibling;
-		if (panel.style.display === 'block') {
-			panel.style.display = 'none';
+		if (panel.style.maxHeight || panel.style.padding) {
+			panel.style.maxHeight = null;
+			panel.style.padding = null;
 		} else {
-			panel.style.display = 'block';
+			panel.style.maxHeight = `${panel.scrollHeight}px`;
+			panel.style.padding = `1rem`;
 		}
 	});
 }
