740a3f030a39179a7f1a4388a32f505f78555952
Aditya Chakraborty <aditya09me@gmail.com>
2022-04-05 12:08:26 +0600

    js modified

--- 16-faq-collapse/app.js
+++ 16-faq-collapse/app.js
@@ -2,11 +2,6 @@ const toggles = document.querySelectorAll('.faq-toggle');
 
 toggles.forEach((toggle) => {
 	toggle.addEventListener('click', () => {
-		removeActiveClass();
 		toggle.parentNode.classList.toggle('active');
 	});
 });
-
-function removeActiveClass() {
-	toggles.forEach((toggle) => toggle.parentNode.classList.remove('active'));
-}
